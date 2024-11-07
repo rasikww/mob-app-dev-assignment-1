a9135f106290f67d855ed8bc2af1d8aaa33c9ae8
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2024-11-07 21:54:52 +0530

    first commit

---
+++ bin/lms.dart
@@ -0,0 +1,5 @@
+import 'package:lms/lms.dart' as lms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lms.calculate()}!');
+}
---
+++ test/lms_test.dart
@@ -0,0 +1,8 @@
+import 'package:lms/lms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
